8eebcee86b83d9e2ddb3fe348b9f5e9f73e9a053
Piotrek <65945270+Piotrek-hub@users.noreply.github.com>
2022-12-13 03:39:08 +0100

    Middle part

--- app/src/main/java/com/example/myapplication/Image.java
+++ app/src/main/java/com/example/myapplication/Image.java
@@ -1,16 +1,26 @@
 package com.example.myapplication;
 
+import android.util.Log;
+
 public class Image {
     int resourceID;
     int counter;
 
     Image(int resourceID) {
         this.resourceID = resourceID;
-        
+
+    }
+
+    void incrementCounter() {
+        this.counter += 1;
     }
 
     int getResourceID() {
+        this.incrementCounter();
         return this.resourceID;
     }
 
+    int getCounter() {
+        return this.counter;
+    }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     ImageView topSectionImage;
@@ -22,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         this.topSectionImage = (ImageView) findViewById(R.id.topImage);
         topSectionImage.setImageResource(imagesWithOrder[currentResource].getResourceID());
+
+        displayImagesInfo();
     }
 
     private void changeResource(int value) {
@@ -38,12 +42,32 @@ public class MainActivity extends AppCompatActivity {
     {
         this.changeResource(-1);
         this.topSectionImage.setImageResource(imagesWithOrder[currentResource].getResourceID());
-
+        displayImagesInfo();
     }
 
     public void onClickNext(View v)
     {
         this.changeResource(1);
         this.topSectionImage.setImageResource(imagesWithOrder[currentResource].getResourceID());
+        displayImagesInfo();
+    }
+
+    private int countAllImagesValues() {
+        int sum = 0;
+        for(Image img : imagesWithOrder) {
+            sum += img.getCounter();
+        }
+        return sum;
+    }
+
+    private void displayImagesInfo() {
+        TextView currentImageValueView = (TextView) findViewById(R.id.currentImageValue);
+        TextView allImagesValueView = (TextView) findViewById(R.id.allImagesValue);
+
+        String currentImageValue = Integer.toString(imagesWithOrder[currentResource].getCounter());
+        currentImageValueView.setText(currentImageValue);
+
+        String allImagesValue = Integer.toString(countAllImagesValues());
+        allImagesValueView.setText(allImagesValue);
     }
 }
